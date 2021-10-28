8e64ffea22a10fcaba33fefcd30ab7530249433d
Oi Shadmon <oshadmon@gmail.com>
2021-10-28 10:21:36 -0700

    mac/linux

--- examples/curl.sh
+++ examples/curl.sh
@@ -3,8 +3,12 @@
 USERNAME=ori
 PASSWORD=test 
 
-AUTH=$(echo -ne "$USERNAME:$PASSWORD" | base64 --wrap 0)
+if [[ `base64 --help  | grep "\--wrap"` ]] 
+then
+    AUTH=$(echo -ne "$USERNAME:$PASSWORD" | base64 --wrap 0)
+else 
+    AUTH=$(echo -ne "$USERNAME:$PASSWORD" | base64 --break 0)
+fi 
 
-curl -X GET 10.0.0.231:2049 -H "command: get status" -H "User-Agent: AnyLog/1.23" -H "Authorization: ${AUTH}" 
-
+curl -X GET 10.0.0.231:2049 -H "command: get status" -H "User-Agent: AnyLog/1.23" -H "Authorization: ${AUTH}" -w "\n" 
  
